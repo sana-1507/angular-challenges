2d3a007685f207481478d73eecb2669093cfe0ec
Sana Aafreen <aafreen1507@gmail.com>
2025-07-24 15:44:03 +0530

    feat(router-input): refactored code to use RouterInput strategy

--- apps/angular/22-router-input/src/app/test.component.ts
+++ apps/angular/22-router-input/src/app/test.component.ts
@@ -1,6 +1,5 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject, Input as RouterInput } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input as RouterInput } from '@angular/core';
 
 @Component({
   selector: 'app-subscription',
@@ -12,7 +11,6 @@ import { ActivatedRoute } from '@angular/router';
   `,
 })
 export default class TestComponent {
-  private activatedRoute = inject(ActivatedRoute);
   @RouterInput() testId!: string;
   @RouterInput() permission!: string;
   @RouterInput() user!: string;
